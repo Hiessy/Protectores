dcd9134303a0c6a3ef84c3138e947d450073d18f
martin <diaz.martin06@gmail.com>
2015-10-29 09:14:29 -0300

    Seguiendo la guia de mvc de mixrosft

---
+++ Protectores/DAL/ProtectoredInitializer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Protectores.Models;
+
+namespace Protectores.DAL
+{
+    public class ProtectoredInitializer : System.Data.Entity.DropCreateDatabaseIfModelChanges<ProtectoresContext>
+    {
+        protected override void Seed(ProtectoresContext context)
+        {
+            var usuarios = new List<Usuario>
+            {
+                new Usuario { Nombre = "Martin", Apellido = "Diaz", Correo = "darkpig@gmail.com", Organizacion = "Martines Unidos", Password = "Tomahawk23", Perfil=Perfil.Protector, Telefono = "98438327" },
+                new Usuario { Nombre = "Agustin", Apellido = "Dib", Correo = "dib@gmail.com", Organizacion = "Agustin Unidos", Password = "Tomahawk22", Perfil=Perfil.Ambos, Telefono = "45363432" },
+               /* new Usuario { },
+                new Usuario { },
+                new Usuario { }*/
+            };
+            usuarios.ForEach(s => context.Usuarios.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
---
+++ Protectores/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Protectores.Models
+{
+    public enum Perfil {
+        Basico, Protector, Adoptador, Ambos
+    }
+    public class Usuario
+    {
+        public int ProtectorID { get; set; }
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Organizacion { get; set; }
+        public string Correo { get; set; }
+        public string Password { get; set; }
+        public string Telefono { get; set; }
+        public Perfil Perfil { get; set; }
+    }
+}
