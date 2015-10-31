ed8fb19190bdeaeacdb3dfa160fcb5baff3a2a39
martin <diaz.martin06@gmail.com>
2015-10-31 11:06:26 -0300

    Implementing Usuario Controler and view

--- Protectores/Models/Usuario.cs
+++ Protectores/Models/Usuario.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
 namespace Protectores.Models
 {
@@ -10,11 +12,16 @@ namespace Protectores.Models
     }
     public class Usuario
     {
-        public int ProtectorID { get; set; }
+        [Key]
+        public int UsuarioID { get; set; }
+
         public string Nombre { get; set; }
         public string Apellido { get; set; }
         public string Organizacion { get; set; }
+
+        [DataType(DataType.EmailAddress)]
         public string Correo { get; set; }
+
         public string Password { get; set; }
         public string Telefono { get; set; }
         public Perfil Perfil { get; set; }
