e27d1ab4d58e2a7ec7d1d18391f40160fbadd40d
martin <diaz.martin06@gmail.com>
2015-11-10 20:26:58 -0300

    Agregando lo de Agustin

---
+++ Protectores/Controllers/AdopcionAnimalController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Protectores.Controllers
+{
+    public class AdopcionAnimalController : Controller
+    {
+        // GET: AdopcionAnimal
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: AdopcionAnimal/List
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        // GET: AdopcionAnimal/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: AdopcionAnimal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: AdopcionAnimal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdopcionAnimal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: AdopcionAnimal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: AdopcionAnimal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: AdopcionAnimal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- Protectores/Models/AdopcionAnimal.cs
+++ Protectores/Models/AdopcionAnimal.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,20 @@ namespace Protectores.Models
 {
     public class AdopcionAnimal
     {
+        [Key]
+        public int id { get; set; }
+
+        public int usuarioId { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [Display(Name = "Nombre de la mascota")]
+        public string nombre { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        [RegularExpression(@"\d{0,20}", ErrorMessage = "La especie debe tener menos de 20 caracteres.")]
+        [Display(Name = "Especie")]
+        public string especie { get; set; }
     }
 }
