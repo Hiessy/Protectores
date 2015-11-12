8f8c2f4b8ab04944ce2aec9679604db7563cd5f2
martin <diaz.martin06@gmail.com>
2015-11-11 21:08:10 -0300

    adding changes to the model

--- Protectores/Models/Usuario.cs
+++ Protectores/Models/Usuario.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.ComponentModel.DataAnnotations;
 
 namespace Protectores.Models
 {
