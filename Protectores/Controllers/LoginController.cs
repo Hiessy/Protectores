7a1558ceb0b6601bffe62fee0218460deb30b346
martin <diaz.martin06@gmail.com>
2015-11-22 11:27:49 -0300

    adding login page

---
+++ Protectores/Controllers/LoginController.cs
@@ -0,0 +1,37 @@
+﻿using Protectores.DAL;
+using Protectores.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Protectores.Controllers
+{
+    public class LoginController : Controller
+    {
+        private ProtectoresContext dc = new ProtectoresContext();
+
+        // GET: Registracion
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(Usuario u)
+        {
+            var v = dc.Usuarios.Where(a => a.Correo.Equals(u.Correo) && a.Password.Equals(u.Password)).FirstOrDefault();
+            if (v != null)
+            {
+                Session["LogedUserID"] = v.Correo.ToString();
+                Session["LogedUserFullname"] = v.Nombre.ToString();
+                return RedirectToAction("Create", "Registracion");
+            }
+
+            return View();
+        }
+
+     }
+}
