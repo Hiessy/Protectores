f6b207f27e6dd57e3d8186c0b4220e521bef526e
AgustinDib <agustindib@gmail.com>
2015-12-05 15:20:22 -0200

    nuevo pargina de registracion

---
+++ Protectores/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Protectores.Models
+{
+    public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [ForeignKey("Adress")]
+        public int AdressId { get; set; }
+        public virtual Adress Adress { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Correo Electrónico")]
+        public string Correo { get; set; }
+
+        [Required]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+    }
+}
