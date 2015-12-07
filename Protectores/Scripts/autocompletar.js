0dbd0918bfc9328adcb902f6fb4add80e2443aa5
Lucho1980 <lucianonatoli@gmail.com>
2015-12-07 13:53:39 -0300

    Autocomplete address

--- Protectores/Scripts/autocompletar.js
+++ Protectores/Scripts/autocompletar.js
@@ -1,29 +1,31 @@
 ﻿function initialize() {
     var input = document.getElementById('pac-input');
     var autocomplete = new google.maps.places.Autocomplete(input);
-    google.maps.event.addListener(autocomplete, 'place_changed', function () {
-        var place = autocomplete.getPlace();
-        document.getElementById('city2').value = place.name;
+﻿}
+   window.onload = initialize;
+   // google.maps.event.addListener(autocomplete, 'place_changed', function () {
+     //   var place = autocomplete.getPlace();
+       // document.getElementById('city2').value = place.name;
         //document.getElementById('cityLat').value = place.geometry.location.lat();
         //document.getElementById('cityLng').value = place.geometry.location.lng();
-        $("<p>" + place.geometry.location.lat() + "</p>").appendTo('#lat');
-        $("<p>" + place.geometry.location.lng() + "</p>").appendTo('#lon');
-        $.ajax({
-            url: '/Location/GetResult',
-            type: 'POST',
-            dataType: 'json',
-            data: { data: "{lat: " + place.geometry.location.lat() + ", lon : " + place.geometry.location.lng() + "}" },
-            success: function () {
-                console.log('exito');
-            },
-            error: function () {
-                console.log('fracaso');
-            }
-        });
+        //$("<p>" + place.geometry.location.lat() + "</p>").appendTo('#lat');
+        //$("<p>" + place.geometry.location.lng() + "</p>").appendTo('#lon');
+        //$.ajax({
+          //  url: '/Location/GetResult',
+        //    type: 'POST',
+         //   dataType: 'json',
+          //  data: { data: "{lat: " + place.geometry.location.lat() + ", lon : " + place.geometry.location.lng() + "}" },
+        //    success: function () {
+          //      console.log('exito');
+        //    },
+        //    error: function () {
+         //       console.log('fracaso');
+          //  }
+    //    });
         //alert("This function is working!");
         //alert(place.name);
         // alert(place.address_components[0].long_name);
 
-    });
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+//    });
+//}
+//google.maps.event.addDomListener(window, 'load', initialize);
